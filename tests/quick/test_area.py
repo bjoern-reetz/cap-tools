583ae10a37308fc75b15ee2e872950fc91d6e684
Björn Reetz <git@bjoern-reetz.de>
2024-05-14 18:56:23 +0200

    feat(cap_tools.models.Area): add geocode_to_dict and geocode_from_dict methods

---
+++ tests/quick/test_area.py
@@ -0,0 +1,20 @@
+from cap_tools.models import Area, Geocode, Value, ValueName
+
+
+def test_geocode_to_from_dict():
+    geocodes_dict = {"foo": "bar", "lorem": "ipsum"}
+    geocodes = [
+        Geocode(ValueName(name), Value(value)) for name, value in geocodes_dict.items()
+    ]
+    area = Area(area_desc="my area", geocodes=geocodes)
+    assert area.geocodes_to_dict() == geocodes_dict
+
+    new_geocodes_dict = {"foo": "baz", "dolor": "sit"}
+    area.geocodes_from_dict(new_geocodes_dict)
+    assert area.geocodes_to_dict() == new_geocodes_dict
+
+    new_geocodes = [
+        Geocode(ValueName(name), Value(value))
+        for name, value in new_geocodes_dict.items()
+    ]
+    assert area.geocodes == new_geocodes
