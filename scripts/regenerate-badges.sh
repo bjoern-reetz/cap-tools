35f38c600e64220e63ba6d48d37f080d8752b229
Björn Reetz <git@bjoern-reetz.de>
2024-06-27 16:49:31 +0200

    ci: reduce some badges to only title with logo

--- scripts/regenerate-badges.sh
+++ scripts/regenerate-badges.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-poetry run -- python -m pybadges --left-text "" --right-text Ruff --left-color grey --right-color "#261230" --logo ./images/logos/ruff.svg --embed-logo > images/ruff.svg
-poetry run -- python -m pybadges --left-text "pre-commit" --right-text enabled --right-color "#0c0" --logo ./images/logos/pre-commit.svg --embed-logo > images/pre-commit.svg
+poetry run -- python -m pybadges --left-text "" --right-text Ruff --right-color "#261230" --logo ./images/logos/ruff.svg --embed-logo > images/ruff.svg
+poetry run -- python -m pybadges --left-text "" --right-text "pre-commit" --right-color "#1f2d23" --logo ./images/logos/pre-commit.svg --embed-logo > images/pre-commit.svg
 poetry run -- python -m pybadges --left-text license --right-text MIT --right-color "#97ca00" > images/license.svg
-poetry run -- python -m pybadges --left-text mypy --right-text checked --right-color "#2a6db2" > images/mypy.svg
+poetry run -- python -m pybadges --left-text "" --right-text mypy --left-color "#1a1c1e" --right-color "#2a6db2" --logo ./images/logos/mypy.svg --embed-logo > images/mypy.svg
 
 if [[ -f .coverage ]]; then
   percent_covered_display=$(poetry run -- coverage json --include "src/*" -o - | jq -r '.totals.percent_covered_display')
