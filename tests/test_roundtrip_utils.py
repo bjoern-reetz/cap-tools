e107abf4cae343a7f5564cab0413847142ccbb8e
Björn Reetz <git@bjoern-reetz.de>
2024-07-20 23:42:51 +0200

    refactor(utils): change split_and_remove_quotes and join_and_maybe_add_quotes to accept positional arguments only
    BREAKING CHANGE: The capability of uting keyword-arguments was removed.

--- tests/test_roundtrip_utils.py
+++ tests/test_roundtrip_utils.py
@@ -10,6 +10,6 @@ from . import strategies as custom_st
 def test_roundtrip_join_and_maybe_add_quotes_split_and_remove_quotes(
     items: list[str],
 ) -> None:
-    value0 = cap_tools.utils.join_and_maybe_add_quotes(items=items)
-    value1 = cap_tools.utils.split_and_remove_quotes(items=value0)
+    value0 = cap_tools.utils.join_and_maybe_add_quotes(items)
+    value1 = cap_tools.utils.split_and_remove_quotes(value0)
     assert items == value1, (items, value1)
